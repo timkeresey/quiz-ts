5e96589411c8d4c9cbfcce264ffc6fb84e36524b
Tim Keresey <tim.keresey@gmail.com>
2020-12-23 14:49:44 -0700

    write component scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+//Components
+import QuestionCard from './components/QuestionCard';
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button classname="start" onclick={startTrivia}>
+        start
+      </button>
+      <p classname="score">Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button classname="next" onClick={nextQuestion}>
+        Next Question
+      </button>  
     </div>
   );
 }
